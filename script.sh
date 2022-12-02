27e004c91b5d2d4e296f8198e92ee60814e82d20
guigui <fth46090@hotmail.fr>
2022-12-02 13:30:40 +0100

    42 commit

---
+++ script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ ! -d "/goinfre/$USER/.brew" ]; then
+  # install homebrew
+  echo "###### INSTALLING HOMEBREW ######"
+  curl -fsSL https://raw.githubusercontent.com/omimouni/42homebrew/master/install-goinfre.sh | zsh
+  echo ""
+fi
+
+
+# install vscode
+echo "###### INSTALLING VSCODE ######"
+VSCODE_VERSION=$(curl -s https://formulae.brew.sh/api/cask/visual-studio-code.json | jq .version -r)
+curl -L https://update.code.visualstudio.com/$VSCODE_VERSION/darwin/stable -o ~/goinfre/vscode.zip
+unzip ~/goinfre/vscode.zip -d ~/goinfre
+rm ~/goinfre/vscode.zip
+xattr -dr com.apple.quarantine ~/goinfre/Visual\ Studio\ Code.app
+mkdir ~/goinfre/code-portable-data
+echo ""
+
+
+#install valgrind
+echo "###### INSTALLING VALGRIND ######"
+brew tap LouisBrunner/valgrind
+brew install --HEAD LouisBrunner/valgrind/valgrind
+echo ""
+
+#install readline 
+echo "###### INSTALLING  READLINE  #####"
+brew install readline
