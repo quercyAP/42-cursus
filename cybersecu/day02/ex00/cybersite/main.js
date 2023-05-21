b8f86b5d6edf614c2ebe6b434dcaea07939bd6b5
quercyAP <fth46090@hotmail.fr>
2023-05-21 20:13:26 +0200

    finish cybersite

---
+++ cybersecu/day02/ex00/cybersite/main.js
@@ -0,0 +1,18 @@
+var cybersecurityTips = [
+    "Utilisez toujours des mots de passe forts",
+    "Mettez régulièrement à jour vos logiciels",
+    "Ne cliquez pas sur des liens douteux",
+    // Ajoutez plus de conseils ici
+];
+
+function loadTips() {
+    var tipsElement = document.getElementById("tips");
+
+    for (var i = 0; i < cybersecurityTips.length; i++) {
+        var listItem = document.createElement("li");
+        listItem.textContent = cybersecurityTips[i];
+        tipsElement.appendChild(listItem);
+    }
+}
+
+window.onload = loadTips;
---
+++ cybersecu/day02/ex00/cybersite/scripts.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById('matrix');
+const context = canvas.getContext('2d');
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+const characters = '0123456789ABCDEF'; // nous utilisons ces caractères
+const columns = canvas.width / 15; 
+
+const drops = [];
+
+for (let x = 0; x < columns; x++) {
+    drops[x] = 1;
+}
+
+function drawMatrix() {
+    context.fillStyle = "rgba(0, 0, 0, 0.05)";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = "#0f0";
+    context.font = "15px Orbitron";
+
+    for (let i = 0; i < drops.length; i++) {
+        const text = characters[Math.floor(Math.random() * characters.length)];
+        
+        context.fillText(text, i * 15, drops[i] * 15);
+
+        if (drops[i] * 15 > canvas.height && Math.random() > 0.975) {
+            drops[i] = 0;
+        }
+        drops[i]++;
+    }
+}
+
+setInterval(drawMatrix, 33);
