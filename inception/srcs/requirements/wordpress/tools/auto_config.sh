81d3e2367fcf9d8e43a48281f5ed18dd4255389b
quercyAP <fth46090@hotmail.fr>
2023-06-02 10:36:02 +0200

    modif inception

--- inception/srcs/requirements/wordpress/tools/auto_config.sh
+++ inception/srcs/requirements/wordpress/tools/auto_config.sh
@@ -2,13 +2,12 @@
 
 set
 
-sleep 10
-
-echo "SQL_DATABASE: ${SQL_DATABASE}"
-echo "SQL_USER: ${SQL_USER}"
-echo "SQL_PASSWORD: ${SQL_PASSWORD}"
+until mysqladmin ping -h"$SQL_HOST" -P"$SQL_PORT" --silent; do
+    echo 'waiting for mysqld to be connectable...'
+    sleep 2
+done
 
 if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
-    wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
+    wp --allow-root config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
                     --dbhost=mariadb:3306 --path='/var/www/wordpress'
 fi
